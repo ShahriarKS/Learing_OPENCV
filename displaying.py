7508b338f87c8d520369c90fd1e585a86547f615
icarusGT <sabicusha707@gmail.com>
2026-05-29 18:03:17 +0600

    Initial commit

---
+++ displaying.py
@@ -0,0 +1,17 @@
+# three functions 
+
+# imshow("title",variable) -> it creates a window to show the image
+# waitkey(0) ->wait untill user presses any key ,then close the window. 0 means infinte wait
+#destroyAllwindows
+
+import cv2
+
+image=cv2.imread("vanG.jpg")
+
+if image is not None : 
+ cv2.imshow("van gogh er chobi",image)
+ cv2.waitKey(0)
+ cv2.destroyAllWindows()
+
+else :
+ print("not showing") 
---
+++ loading.py
@@ -0,0 +1,13 @@
+import cv2
+
+print(cv2.__version__)
+
+#image reading
+
+image=cv2.imread("vanG.jpg")
+
+if image is None :
+    print("error : image is not found")
+
+else:
+    print("image loaded successfully")
